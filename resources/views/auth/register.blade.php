36a03249c397f44cebfe5cdb9b5f3bdd2bfa6dea
Jeden1102 <bum123@o2.pl>
2021-08-29 20:37:29 +0200

    test chgange

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,7 +19,7 @@
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $message }}x</strong>
                                     </span>
                                 @enderror
                             </div>
